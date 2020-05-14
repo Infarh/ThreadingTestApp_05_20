8d6a6ab3e14a4ca93a85fb900c47fb1d19a1283f
Infarh <shmachilin@yandex.ru>
2020-05-14 12:26:07 +0300

    Просмотр информации о процессах в ОС

--- ThreadingTestApp/Program.cs
+++ ThreadingTestApp/Program.cs
@@ -12,9 +12,25 @@ namespace ThreadingTestApp
     {
         static void Main(string[] args)
         {
-          
+            ViewProcessConformation();
+
             Console.WriteLine("Главный поток завершён!");
             Console.ReadLine();
+        }
+
+        private static void ViewProcessConformation()
+        {
+            var processes = Process.GetProcesses();
+            foreach (var process in processes)
+                try
+                {
+                    Console.WriteLine("[pid:{0}] {1}",
+                        process.Id, process.ProcessName);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
         }
     }
 }
