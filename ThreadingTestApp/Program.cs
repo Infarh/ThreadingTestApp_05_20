138cac62f54b9247902855dab18aae03b4bad898
Infarh <shmachilin@yandex.ru>
2020-05-14 12:25:31 +0300

    Добавьте файлы проекта.

---
+++ ThreadingTestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading;
+using System.Diagnostics;
+
+namespace ThreadingTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Главный поток завершён!");
+            Console.ReadLine();
+        }
+    }
+}
